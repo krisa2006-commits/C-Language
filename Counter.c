09091eee2577f8901244e54e9644cb487dfbba29
krisa2006-commits <krishachaniyara2706@gmail.com>
2025-12-26 16:40:54 +0530

    Refactor digit counting program in Counter.c

--- Counter.c
+++ Counter.c
@@ -2,19 +2,14 @@
 
 int main()
 {
-    long long i;
-    int count = 0;
+    int i, count;
 
     printf("Enter any number: ");
-    if (scanf("%lld", &i) != 1);
+    scanf("%d", &i);
 
-    if (i == 0) {
+    if (i == 0){
         count = 1;
     } else {
-
-        if (i < 0) {
-            i = -i;
-        }
         
         while (i != 0) {
             i /= 10;
